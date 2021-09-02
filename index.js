1fcdafe9b589fa906fac3da28de9b6098f61472a
alibayramcavus <46304170+alibayramcavus@users.noreply.github.com>
2021-09-02 14:50:43 +0300

    deneme

--- index.js
+++ index.js
@@ -21,5 +21,5 @@ app.get("/", (req, res) => {
 })
 const port = process.env.PORT || 80
 app.listen(port, () => {
-    console.log("Server running on port: " + port)
+    console.log("Server running on " + port)
 })
